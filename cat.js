b2868faa80c6ca4fa74b438bd4cd0bc3da88cbfb
chongnguyen <chongnguyen19@gmail.com>
2020-03-14 09:00:39 +0700

    add method eat for cat function

--- cat.js
+++ cat.js
@@ -1,6 +1,10 @@
 function Cat(name, weight){
   this.name = name;
   this.weight = weight;
+  this.stomach = [];
+}
+Cat.prototype.eat = function(mouse){
+  this.stomach.push(mouse);
 }
 
 module.exports = Cat;

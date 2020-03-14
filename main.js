6cb0d33a0e8189d02804bd435b7b4f6aab4c0527
chongnguyen <chongnguyen19@gmail.com>
2020-03-14 14:26:37 +0700

    add content for horse

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 var Cat = require('./cat');
 var Mouse = require('./mouse');
 var Dog = require('./dog');
+var Horse = require('./horse');
 
 var mickey = new Mouse('mickey', 'orange');
 var tom  = new Cat('tom', 2);
 var dog = new Dog();
+var horse = new Horse('van', 100);
+
 try {
   tom.eat(dog);
 } catch (error) {
   console.log(error);
 }
 
+horse.eat();
 console.log(tom);

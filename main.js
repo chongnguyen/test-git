ba56e1e3a9b81865466ff1bf76f3f3345e87d9a2
chongnguyen <chongnguyen19@gmail.com>
2020-03-14 09:24:00 +0700

    add file main.js, method dead for Mouse

---
+++ main.js
@@ -0,0 +1,1 @@
+var Mouse = require('./mouse');
--- mouse.js
+++ mouse.js
@@ -1,6 +1,11 @@
 function Mouse(name, color){
   this.name = name;
   this.color = color;
+  this.dead = false;
+}
+
+Mouse.prototype.die = function(){
+  this.dead = true;
 }
 
 module.exports = Mouse;
